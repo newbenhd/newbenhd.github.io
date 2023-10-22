6ddf14bdb9f74012fdaf7733cc1aaf0dc2d268cb
Hyeondong Lee <hyeondonglee@hyeondongs-air.lan>
2023-10-21 22:33:01 -0700

    add base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/newbenhd.github.io",
+  assetPrefix: "/newbenhd.github.io",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
